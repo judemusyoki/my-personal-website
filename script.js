82b07d8a67b480daaae323b9db8a31b3f82512e2
jmusyoki <jmusyoki@wastehero.io>
2021-02-08 22:03:12 +0100

    created testimonials section

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 const textElIntro = document.getElementById('intro');
 const textElAboutMe = document.getElementById('aboutMe');
 const textElSkills = document.getElementById('skills');
+const textElTestimonials = document.getElementById('testimonials');
 
 const textIntro = 'Hi, I am Jude Musyoki...';
 const textAboutMe = 'A little about me...';
 const textSkills = 'i build with...';
+const textTestimonials = 'Working with me...';
 
 let idxIntro = 1;
 let idxAboutMe = 1;
 let idxSkills = 1;
+let idxTestimonials = 1;
 speed = 150;
 
 writeIntro();
 writeAboutMe();
 writeSkills();
+writeTestimonials();
 
 function writeIntro() {
   textElIntro.innerText = textIntro.slice(0, idxIntro);
@@ -40,13 +44,76 @@ function writeAboutMe() {
 }
 
 function writeSkills() {
-  textElSkills.innerText = textSkills.slice(0, idxSkills);
+  textElSkills.innerText = textTestimonials.slice(0, idxSkills);
 
   idxSkills++;
 
-  if (idxSkills > textSkills.length) {
+  if (idxSkills > textTestimonials.length) {
     idxSkills = 1;
   }
 
   setTimeout(writeSkills, speed);
 }
+
+function writeTestimonials() {
+  textElTestimonials.innerText = textTestimonials.slice(0, idxTestimonials);
+
+  idxTestimonials++;
+
+  if (idxTestimonials > textTestimonials.length) {
+    idxTestimonials = 1;
+  }
+
+  setTimeout(writeTestimonials, speed);
+}
+
+/* Testimoinals */
+
+const testimonialsContainer = document.querySelector('.testimonials');
+const testimonial = document.querySelector('.testimonial');
+const userImage = document.querySelector('.userImage');
+const username = document.querySelector('.username');
+const role = document.querySelector('.role');
+const companyWebsite = document.querySelector('.companyWebsite');
+
+const testimonials = [
+  {
+    name: 'Anders Hinrichs',
+    image: './images/user_images/anders.png',
+    position: 'Director Business Development',
+    company: 'WasteHero',
+    website: 'https://www.wastehero.io/en/',
+    text:
+      'Jude quickly learned and adapted to our tech stack, which meant that already after a couple of weeks Jude was ready to push code to production. As a person, Jude is very social and outgoing. This was essential to his excellent collaboration with both technical and commercial-minded colleagues.',
+  },
+  {
+    name: 'Vincentius Mathius Nielsen',
+    image: './images/user_images/vinc.jpg',
+    position: 'Managing Director',
+    company: 'Cafe Smaglos',
+    website: 'https://xn--cafesmagls-8cb.com/',
+    text:
+      'Working with Jude has been a real pleasure. He has helped me develop a good and smooth homepage for my café. We have had a few, but very constructive meetings, where we discussed the project, and how the work-process would work out. I am not a tech geek, therefore I expressed what I felt was important in the homepage as a café owner. Jude listened to my needs, and created a functional and good looking website that appeals to the café’s theme and vibe.',
+  },
+];
+
+let idx = 1;
+
+function updateTestimonial() {
+  const { name, image, position, company, website, text } = testimonials[idx];
+
+  testimonial.innerHTML = text;
+  userImage.src = image;
+  username.innerHTML = name;
+  role.innerHTML = position;
+  companyWebsite.innerHTML = company;
+  companyWebsite.href = website;
+
+  idx++;
+
+  if (idx > testimonials.length - 1) {
+    idx = 0;
+  }
+}
+
+setInterval(updateTestimonial, 7000);
