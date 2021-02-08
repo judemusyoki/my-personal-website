67389ebb14f4a46700fa5c748bcf30a403c7b3a4
jmusyoki <jmusyoki@wastehero.io>
2021-02-08 21:00:07 +0100

    created skills section

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 const textElIntro = document.getElementById('intro');
 const textElAboutMe = document.getElementById('aboutMe');
+const textElSkills = document.getElementById('skills');
 
 const textIntro = 'Hi, I am Jude Musyoki...';
 const textAboutMe = 'A little about me...';
+const textSkills = 'i build with...';
 
 let idxIntro = 1;
 let idxAboutMe = 1;
-speed = 100;
+let idxSkills = 1;
+speed = 150;
 
 writeIntro();
 writeAboutMe();
+writeSkills();
 
 function writeIntro() {
   textElIntro.innerText = textIntro.slice(0, idxIntro);
@@ -33,4 +37,16 @@ function writeAboutMe() {
   }
 
   setTimeout(writeAboutMe, speed);
+}
+
+function writeSkills() {
+  textElSkills.innerText = textSkills.slice(0, idxSkills);
+
+  idxSkills++;
+
+  if (idxSkills > textSkills.length) {
+    idxSkills = 1;
+  }
+
+  setTimeout(writeSkills, speed);
 }
